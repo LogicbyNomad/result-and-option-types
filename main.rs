f33399f44334733203f4f9a92fd771c51b6546a4
Alexander T <Alexander.Thomas_2026@gmail.com>
2019-08-11 14:02:13 -0400

    result and option types

---
+++ main.rs
@@ -0,0 +1,18 @@
+use std::collections::HashMap;
+
+fn main() {
+
+    let mut hm = HashMap::new();
+
+    hm.insert(3, "Hello");
+    hm.insert(5, "World");
+
+    let r =  hm.get(&4).unwrap_or(&"NoString");
+    println!("{}", r);
+    
+
+    match "3".parse::<f32>() {
+        Ok(v)=>println!("OK - {}", v),
+        Err(e)=>println!("Error - {}", e),
+    }
+}
