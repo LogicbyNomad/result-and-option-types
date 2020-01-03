a566c97722f29b202b53309462f984f420e5d521
Alexander T <Alexander.Thomas_2026@gmail.com>
2020-01-03 15:51:36 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -4,14 +4,14 @@ fn main() {
 
     let mut hm = HashMap::new();
 
-    hm.insert(3, "Hello");
+    hm.insert(3 , "Hello");
     hm.insert(5, "World");
 
     let r =  hm.get(&4).unwrap_or(&"NoString");
     println!("{}", r);
     
 
-    match "3".parse::<f32>() {
+    match "5".parse::<f32>() {
         Ok(v)=>println!("OK - {}", v),
         Err(e)=>println!("Error - {}", e),
     }
